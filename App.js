e2cd640e54358e96cefa51a5912bf658710e8a0a
Mahesh <maheshgowda3501@gmail.com>
2023-08-26 21:45:13 +0530

    commit

--- App.js
+++ App.js
@@ -1,36 +1,66 @@
-import React from 'react';
-import { Platform } from 'react-native';
+import React, { useState } from 'react';
+import { Keyboard, Platform } from 'react-native';
 import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import Task from './components/Task';
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
+    </View>
+  );
+};
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    if (task.trim() !== '') {
+      setTaskItems([...taskItems, task]);
+      setTask('');
+    }
+  };
+
+  const completeTask = (index) => {
+    const itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
-
-      {/* Today's Tasks */}
       <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks</Text>
-
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
         <View style={styles.items}>
-          {/* This is where the tasks will go! */}
-          <Task text={'Task 1'}/>
-          <Task text={'Task 2'} />
+          {taskItems.map((item, index) => (
+            <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+              <Task text={item} />
+            </TouchableOpacity>
+          ))}
         </View>
       </View>
 
       <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "padding"} // Use "padding" for Android as well
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.writeTaskWrapper}
       >
-        <TextInput style={styles.input} placeholder={'write a task'}/>
-
-        <TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task'}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={handleAddTask}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
-
     </View>
   );
 }
@@ -38,7 +68,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: '#F8F8F8',
   },
   tasksWrapper: {
     paddingTop: 80,
@@ -46,20 +76,75 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    color: '#333333',
   },
   items: {
-    marginTop: 30
+    marginTop: 30,
   },
   writeTaskWrapper: {
     position: 'absolute',
-    bottom: 60,
+    bottom: 20,
     width: '100%',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     alignItems: 'center',
+    paddingHorizontal: 20,
   },
-  input: {},
-  addWrapper: {},
-  addText: {},
+  input: {
+    flex: 1,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#DDDDDD',
+    marginRight: 10,
+  },
+  addWrapper: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#DDDDDD',
+  },
+  addText: {
+    fontSize: 24,
+    color: '#558CF6',
+  },
+  item: {
+    backgroundColor: '#FFFFFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  itemText: {
+    maxWidth: '80%',
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#558CF6',
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: '#558CF6',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
 });
