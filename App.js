0d7f15c8fcf82cbb67b4f50306edba0495e3e587
Mahesh <maheshgowda3501@gmail.com>
2023-08-29 16:39:24 +0530

    commit

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const Task = (props) => {
     </View>
   );
 };
-{/*g */}
+{/* */}
 export default function App() {
   const [task, setTask] = useState('');
   const [taskItems, setTaskItems] = useState([]);
